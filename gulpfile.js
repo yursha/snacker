7ea29cfb5bd468da558738e14bafd31de1095a2f
Alex Yursha <alexyursha@gmail.com>
2015-04-04 16:23:04 +0300

    Set up gulp to precompile JSX files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
 var mocha = require('gulp-mocha');
+var react = require('gulp-react');
 
-gulp.task('default', function() {
+gulp.task('test', function() {
   return gulp.src(['test/test-*.js'], { read: false })
     .pipe(mocha({
       reporter: 'spec',
       globals: {
         should: require('chai')
       }
-    }));
-});
+    }))
+})
+
+/*
+ * Precompile JSX templates on the server
+ */
+gulp.task('jsx', function() {
+  return gulp.src('components-src/*')
+    .pipe(react())
+    .pipe(gulp.dest('public/scripts/components'))
+})
