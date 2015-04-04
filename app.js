32949860891743c424b9c01cd98f61c633c66002
Alex Yursha <alexyursha@gmail.com>
2015-04-04 16:26:02 +0300

    Cleanup after express-generator

--- app.js
+++ app.js
@@ -1,60 +1,76 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/*
+ * This file configures Express application.
+ *
+ * Please note, that Express built-in view rendering is not used. The problem is
+ * that it doesn't allow streaming and feature-based templates organization in
+ * the file system.
+ * @see https://strongloop.com/strongblog/bypassing-express-view-rendering-for-speed-and-modularity/
+ */
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var http = require('http')
+var util = require('util')
+var debug = require('debug')('snacker:server')
 
-var app = express();
+var routes = require('./routes/index')
+var users = require('./routes/users')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var app = express()
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', routes);
-app.use('/users', users);
+/*
+ * Set up `static` middleware to serve static assets from the `./public` folder.
+ *
+ * @see http://expressjs.com/api.html#express.static
+ */
+app.use(express.static('public'))
 
-// catch 404 and forward to error handler
+app.use('/', function(req, res, next) {
+  res.sendFile('./views/index.html', {root: __dirname})
+})
+app.use('/users', users)
+
+/*
+ * 404 'Page Not Found' handler.
+ *
+ * This is the last normal request handler that is registered with Express.
+ * This means it will be called only if no other routes matched.
+ */
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  res.status(404).send('Sorry. Can\'t find that!')
+})
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+/*
+ * Error handler.
+ *
+ * If Express encounters an error while handling the request this handler
+ * will be invoked.
+ */
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+  console.error(err.stack)
+  res.status(err.status || 500).send('Oops... Something went wrong!')
+})
+
+/**
+ * Get port from environment and store in Express.
+ */
+var port = process.env.PORT || 3000
+app.set('port', port)
 
 
-module.exports = app;
+var server = app.listen(port, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Snacker is listening on ' + host + ':' + port)
+})
