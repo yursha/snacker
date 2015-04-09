a34ffd9b1b881bd6c4d17b0498ab72ff5cdef962
Alex Yursha <alexyursha@gmail.com>
2015-04-09 12:37:05 +0300

    Remove unused modules. Add `test` step to `build` task

--- server/main.js
+++ server/main.js
@@ -9,9 +9,6 @@
 
 var express = require('express')
 var path = require('path')
-var favicon = require('serve-favicon')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
 var http = require('http')
 var util = require('util')
 
@@ -22,12 +19,6 @@ var index = require('./routes/index')
 var upload = require('./routes/upload')
 
 var app = express()
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
 
 /*
  * Set up `static` middleware to serve static assets.
