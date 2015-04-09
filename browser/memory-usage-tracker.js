1a87d31f03ea6b8a5e8ee2ccde605dc3bf49c618
Alex Yursha <alexyursha@gmail.com>
2015-04-10 01:23:41 +0300

    Plot 1-minute memory usage samples median

--- browser/memory-usage-tracker.js
+++ browser/memory-usage-tracker.js
@@ -1,9 +1,41 @@
+/*
+ * Polls server for memory usage
+ */
+
 var $ = require('jquery')
+var scatterPlot = require('./scatter-plot')
 
 var samples = []
+var currentMinute = new Date().getMinutes()
 
 module.exports = function() {
-	$.get('/memory', '', function (data, status) {
-		console.log('data = ' + data + ', status = ' + status)
+	$.get('/memory', '', function (data) {
+		var now = new Date().getMinutes()
+		if (now > currentMinute) {
+			var medianValue = median(samples)
+			plotMedian(medianValue, currentMinute)
+			currentMinute = now
+			samples = []
+		} else {
+			samples.push(data)
+		}
 	})
+}
+
+/*
+ * Calculates a median for an array of numbers
+ * 
+ * @parram {data} array of numbers
+ */
+function median(data) {
+	if (data.length) {
+		return data.sort()[Math.floor(data.length / 2)]
+	} else {
+		return 0
+	}
+}
+
+function plotMedian(median, minute) {
+    scatterPlot.config.data.push({"isFile": false, "minute": minute, "memoryUsed": median, "processingTime": 1})
+    scatterPlot.renderTo('#scatter')
 }
